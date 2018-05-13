eca4e0d41fa4df80d2ea07e20cd87b93bcdc0460
denys furman <furik1102@gmail.com>
2018-05-13 22:01:02 +0300

    search

--- app/js/common.js
+++ app/js/common.js
@@ -1,27 +1,20 @@
 
 
 
+jQuery(document).ready(function($){
 
-        //serch
-        $('.control , .icon-material-search , icon-material-search img , .btn-material').click( function(){
-            $('body').addClass('search-active');
-            $('.input-search').focus();
-            $('.icon-close').fadeIn();
-        });
-
-        $('.icon-close').click( function(){
-            $('body').removeClass('search-active');
-            $('.icon-close').fadeOut();
-        });
-
-
-
-
-
-
-
-
+    $('.control , .icon-material-search , .icon-material-search img , .btn-material').click( function(){
+        $('body').addClass('search-active');
+        $('.input-search').focus();
+        $('.icon-close').fadeIn();
     });
+
+    $('.icon-close').click( function(){
+        $('body').removeClass('search-active');
+        $('.icon-close').fadeOut();
+    });
+
+});
 
 
 
